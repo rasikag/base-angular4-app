e14eddc865e00f6efc10d9abe396ac1cb1dfd1fa
rasikag <rasikagayan90@gmail.com>
2018-09-29 22:42:54 +0800

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
@@ -10,10 +11,12 @@ export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe('My Test Recipe',
                     'This is my recipe description',
-                    'https://www.howtocookthat.net/public_html/wp-content/uploads/2017/12/P2390584-550x413.jpg?x19907'),
+                    'https://www.howtocookthat.net/public_html/wp-content/uploads/2017/12/P2390584-550x413.jpg?x19907',
+                    [new Ingredient('My Ingredient 01', 5), new Ingredient('My Ingredient 02', 6)]),
         new Recipe('My Test Recipe',
                     'This is my recipe description',
-                    'https://www.howtocookthat.net/public_html/wp-content/uploads/2017/12/P2390584-550x413.jpg?x19907')
+                    'https://www.howtocookthat.net/public_html/wp-content/uploads/2017/12/P2390584-550x413.jpg?x19907',
+                    [new Ingredient('My Ingredient 03', 5), new Ingredient('My Ingredient 04', 6)])
       ];
 
     getRecipe() {
